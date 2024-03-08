cab89bd5e12c3fcf1593cdcbf5833feda0b6d659
AntonHedlundFG <Anton.Hedlund@edu.futuregames.nu>
2024-03-08 12:20:51 +0100

    Shooting cooldown implemented, server-side

--- Assets/Scripts/Player/FiringAction.cs
+++ Assets/Scripts/Player/FiringAction.cs
@@ -3,6 +3,7 @@ using System.Collections;
 using System.Collections.Generic;
 using Unity.Netcode;
 using UnityEngine;
+using UnityEngine.PlayerLoop;
 
 public class FiringAction : NetworkBehaviour
 {
@@ -11,6 +12,9 @@ public class FiringAction : NetworkBehaviour
     [SerializeField] GameObject serverSingleBulletPrefab;
     [SerializeField] Transform bulletSpawnPoint;
 
+    private NetworkVariable<bool> _isShooting = new NetworkVariable<bool>(false, NetworkVariableReadPermission.Everyone, NetworkVariableWritePermission.Owner);
+    private float _lastShootTime = 0.0f; //Used by client and server, separately.
+    [SerializeField][Range(0.1f, 10.0f)] private float _shootingCooldownSeconds = 1.0f; 
 
     public override void OnNetworkSpawn()
     {
@@ -19,35 +23,44 @@ public class FiringAction : NetworkBehaviour
 
     private void Fire(bool isShooting)
     {
-
-        if (isShooting)
-        {
-            ShootLocalBullet();
-        }
+        _isShooting.Value = isShooting;
     }
 
-    [ServerRpc]
-    private void ShootBulletServerRpc()
+    private void Update()
     {
-        GameObject bullet = Instantiate(serverSingleBulletPrefab, bulletSpawnPoint.position, bulletSpawnPoint.rotation);
-        Physics2D.IgnoreCollision(bullet.GetComponent<Collider2D>(), transform.GetComponent<Collider2D>());
-        ShootBulletClientRpc();
+        if (!_isShooting.Value) return;
+        if (Time.time < _lastShootTime + _shootingCooldownSeconds) return; //Still on cooldown
+
+        if (IsOwner)
+        {
+            ShootLocalBullet(); //Fire predictive shot if local cooldown is up.
+        }
+        if (IsServer)
+        {
+            //Fire actual shot and notify other clients if server cooldown is up.
+            ShootServerBullet(); 
+            ShootLocalBulletClientRpc();
+        }
+
+        _lastShootTime = Time.time;
     }
 
     [ClientRpc]
-    private void ShootBulletClientRpc()
+    private void ShootLocalBulletClientRpc()
     {
-        if (IsOwner) return;
-        GameObject bullet = Instantiate(clientSingleBulletPrefab, bulletSpawnPoint.position, bulletSpawnPoint.rotation);
-        Physics2D.IgnoreCollision(bullet.GetComponent<Collider2D>(), transform.GetComponent<Collider2D>());
-
+        if (IsOwner) return; //Owner has hopefully already fired a predictive shot.
+        ShootLocalBullet();
     }
 
     private void ShootLocalBullet()
     {
         GameObject bullet = Instantiate(clientSingleBulletPrefab, bulletSpawnPoint.position, bulletSpawnPoint.rotation);
         Physics2D.IgnoreCollision(bullet.GetComponent<Collider2D>(), transform.GetComponent<Collider2D>());
+    }
 
-        ShootBulletServerRpc();
+    private void ShootServerBullet()
+    {
+        GameObject bullet = Instantiate(serverSingleBulletPrefab, bulletSpawnPoint.position, bulletSpawnPoint.rotation);
+        Physics2D.IgnoreCollision(bullet.GetComponent<Collider2D>(), transform.GetComponent<Collider2D>());
     }
 }
--- README.md
+++ README.md
@@ -10,3 +10,8 @@ A locally managed TextMesh which [displays players' usernames](Assets/Scripts/Pl
 A pre-placed, non-respawnable [health pack](Assets/Scripts/Mines/HealthPack.cs), with a server-managed trigger that [restores HP](Assets/Scripts/Player/Health.cs#L26-L30). 
 
 ![Health Packs](Pics/HealthPack.gif)
+
+## Task 5. Shot Timer (1pt)
+A shot cooldown managed by server. 
+
+The previous solution allowed the client to make shooting RPC's to the server at any desired speed. [My new solution](Assets/Scripts/Player/FiringAction.cs) uses an _isShooting bool, managed by the owning client, which the server reads to fire actual bullets. The owning client makes local predictive shots, but it is the server's cooldown timer that determines actual outcomes.
